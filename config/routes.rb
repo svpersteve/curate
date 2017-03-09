24ce31299f35940d76fa74d8771f68336174ec0f
Steve Brewer <steve.brewer@futurelearn.com>
2017-03-09 22:13:23 +0000

    Create user sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#index'
+
+  get   '/login', to: 'sessions#new', as: :login
+  match '/auth/:provider/callback', via: [:get, :post], to: 'sessions#create'
+  match '/auth/failure', via: [:get, :post], to: 'sessions#failure'
 end
