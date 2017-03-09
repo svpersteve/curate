af0bd24def034d64086d247a9e4e1dc0cdba0d55
Steve Brewer <steve.brewer@futurelearn.com>
2017-03-09 19:42:43 +0000

    Install database cleaner to clean test db

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -15,3 +15,23 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
 end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+  config.before(:each, multithreaded: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+  config.before(:each, js_with_silent_errors: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
