270791698fa35454d5af6ad8f7c38565c73e9daa
Steve Brewer <steve.brewer@futurelearn.com>
2017-03-10 08:18:36 +0000

    Generate Devise views

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,5 +1,3 @@
-# Use this hook to configure devise mailer, warden hooks and so forth.
-# Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
@@ -240,7 +238,7 @@ Devise.setup do |config|
   #
   # The "*/*" below is required to match Internet Explorer requests.
   # config.navigational_formats = ['*/*', :html]
-
+  config.scoped_views = true
   # The default HTTP method used to sign out a resource. Default is :delete.
   config.sign_out_via = :delete
 
