2306945e96a2999d80a97554ef0ee57f8214ba8c
Steve Brewer <steve.brewer@futurelearn.com>
2017-03-11 18:38:54 +0000

    Add feature helper for logged in users

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -15,6 +15,8 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
 
   config.before(:each) do
     DatabaseCleaner.strategy = :transaction
---
+++ spec/support/feature_support.rb
@@ -0,0 +1,19 @@
+module FeatureSupport
+  module ClassMethods
+    def as_a_logged_in_user(&block)
+      context 'as a logged in user' do
+        let(:user) { FactoryGirl.create(:user, email: 'user@example.com', name: 'My name') }
+
+        before do
+          login_as user
+        end
+
+        instance_eval(&block)
+      end
+    end
+
+  RSpec.configure do |config|
+    config.include self, type: :feature
+    config.extend ClassMethods, type: :feature
+  end
+end
