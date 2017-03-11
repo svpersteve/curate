787630c75557243ff0d9bea0d70f903264293ea9
Steve Brewer <steve.brewer@futurelearn.com>
2017-03-11 17:45:21 +0000

    Create profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "callbacks", registrations: "registrations", passwords: "passwords", sessions: "sessions" }
   root to: 'pages#index'
+
+  resources :users
 end
