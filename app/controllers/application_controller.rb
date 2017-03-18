b13a62a0f0187f95f5afc8e68893e7d6facf1d93
Steve Brewer <steve.brewer@futurelearn.com>
2017-03-18 15:52:20 +0000

    Fix Instagram login bug
    The after_filter that stores the user's previous URL should probably be after
    the protect_from_forgery, as this was generating a CSRF error.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,38 +1,20 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with name: ENV['HTTP_USERNAME'], password: ENV['HTTP_PASSWORD']
-  before_action :store_current_location, unless: :devise_controller?
+  protect_from_forgery with: :exception
   after_filter :store_location
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, notice: exception.message
   end
 
-  protect_from_forgery with: :exception
-
-  def store_current_location
-    store_location_for(:user, request.url)
+  if Rails.env.production?
+    force_ssl
   end
 
   def store_location
-    return unless request.get?
-    if (request.path != "/users/sign_in" &&
-        request.path != "/users/sign_up" &&
-        request.path != "/users/password/new" &&
-        request.path != "/users/password/edit" &&
-        request.path != "/users/confirmation" &&
-        request.path != "/users/sign_out" &&
-        !request.xhr?)
-      session[:previous_url] = request.fullpath
-    end
+    session[:previous_url] = request.fullpath unless request.fullpath =~ %r{/users}
   end
 
   def after_sign_in_path_for(resource)
     session[:previous_url] || root_path
-  end
-
-  private
-
-  def after_sign_out_path_for(resource)
-    request.referrer || root_path
   end
 end
