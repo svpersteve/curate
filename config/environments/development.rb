dba8f2b907821e6a04cf6734f62a29f599494d7c
Steve Brewer <steve.brewer@futurelearn.com>
2017-03-18 15:32:08 +0000

    Add forgotten password link and setup mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,6 +28,7 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
 
   config.action_mailer.perform_caching = false
 
@@ -52,4 +53,17 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.perform_deliveries = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default charset: "utf-8"
+  config.action_mailer.smtp_settings = {
+    port:           '587',
+    address:        'smtp.sendgrid.net',
+    user_name:      ENV['SENDGRID_USERNAME'],
+    password:       ENV['SENDGRID_PASSWORD'],
+    domain:         'heroku.com',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+  ActionMailer::Base.delivery_method = :smtp
 end
