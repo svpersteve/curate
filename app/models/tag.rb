27cab80687505a3002a45fe7c9a60a3c4e896da6
Steve Brewer <steve.brewer@futurelearn.com>
2017-03-18 12:50:18 +0000

    Add tag details and related posts to tag show
    Also link tags to the show view

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,7 +2,7 @@ class Tag < ApplicationRecord
   belongs_to :creator, class_name: 'User'
 
   has_many :post_tags
-  has_many :tags, through: :post_tags
+  has_many :posts, through: :post_tags
 
   extend FriendlyId
   friendly_id :name, use: [:slugged, :finders]
