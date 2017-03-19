1d1c05e8bdcc411c6973b46596e78d0333e34685
Steve Brewer <steve.brewer@futurelearn.com>
2017-03-19 19:42:39 +0000

    Fix broken home feed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :find_user, except: [:index]
+  before_action :find_user, except: [:index, :home]
 
   def show
     @user = User.find(params[:id])
