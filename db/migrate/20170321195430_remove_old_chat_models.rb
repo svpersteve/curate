57a76d2660a485b12c8d2c1235c7ed22142151ff
Steve Brewer <steve.brewer@futurelearn.com>
2017-03-22 09:02:16 +0000

    Remove old chat models

---
+++ db/migrate/20170321195430_remove_old_chat_models.rb
@@ -0,0 +1,22 @@
+class RemoveOldChatModels < ActiveRecord::Migration[5.0]
+  def up
+    drop_table :messages
+    drop_table :chat_rooms
+  end
+
+  def down
+    create_table :chat_rooms do |t|
+      t.string :title
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+    create_table :messages do |t|
+      t.text :body
+      t.references :user, foreign_key: true
+      t.references :chat_room, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
