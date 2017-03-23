7fa8b1526f5b3c98ac60f944c1dce26b73aa7778
Steve Brewer <steve.brewer@futurelearn.com>
2017-03-23 23:41:27 +0000

    Order home page posts by date published

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def index
-    @featured_posts = Post.published.featured
+    @featured_posts = Post.published.featured.order('published_at desc')
   end
 end
