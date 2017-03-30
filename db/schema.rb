b786310d5d25fd9d1ec042ccd27200a387dea28d
Steve Brewer <steve.v.brewer@gmail.com>
2017-03-30 21:42:48 +0100

    Add conversation to messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170330203832) do
+ActiveRecord::Schema.define(version: 20170330204224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -102,8 +102,10 @@ ActiveRecord::Schema.define(version: 20170330203832) do
   create_table "messages", force: :cascade do |t|
     t.integer  "user_id"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.integer  "conversation_id"
+    t.boolean  "read",            default: false
     t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
   end
 
