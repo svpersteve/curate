31ed0a163ee2597346e498f6e41111d2e62bfe40
Steve Brewer <steve.v.brewer@gmail.com>
2017-03-30 23:25:45 +0100

    wip

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,15 +2,14 @@ class ConversationsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @users = User.all
-    @conversations = Conversation.all
+    @conversations = current_user.conversations
   end
 
   def create
-    if Conversation.between(params[:sender_id],params[:recipient_id]).present?
-      @conversation = Conversation.between(params[:sender_id], params[:recipient_id]).first
+    if Conversation.between(sender_id: current_user.id, recipient_id: params[:recipient_id]).present?
+      @conversation = Conversation.between(current_user, @user).first
     else
-      @conversation = Conversation.create!(conversation_params)
+      @conversation = Conversation.create!(sender: current_user, recipient_id: params[:recipient_id])
     end
     redirect_to conversation_messages_path(@conversation)
   end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,19 +3,23 @@ class MessagesController < ApplicationController
 
   def index
     @messages = @conversation.messages
+
     if @messages.length > 10
       @over_ten = true
       @messages = @messages[-10..-1]
     end
+
     if params[:m]
       @over_ten = false
       @messages = @conversation.messages
     end
+
     if @messages.last
       if @messages.last.user_id != current_user.id
         @messages.last.read = true;
       end
     end
+
     @message = @conversation.messages.new
   end
 
@@ -25,6 +29,8 @@ class MessagesController < ApplicationController
 
   def create
     @message = @conversation.messages.new(message_params)
+    @message.user = current_user
+
     if @message.save
       redirect_to conversation_messages_path(@conversation)
     end
@@ -33,7 +39,7 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-    params.require(:message).permit(:body, :user_id)
+    params.require(:message).permit(:body)
   end
 
   def find_conversation
