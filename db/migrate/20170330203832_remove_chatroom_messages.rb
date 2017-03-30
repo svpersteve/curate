2bb1023e0dcb10fcac3d2d86c3412bab15178445
Steve Brewer <steve.v.brewer@gmail.com>
2017-03-30 21:42:02 +0100

    Remove chatrooms from messages

---
+++ db/migrate/20170330203832_remove_chatroom_messages.rb
@@ -0,0 +1,5 @@
+class RemoveChatroomMessages < ActiveRecord::Migration[5.0]
+  def change
+    remove_column :messages, :chatroom_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170330203421) do
+ActiveRecord::Schema.define(version: 20170330203832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -100,12 +100,10 @@ ActiveRecord::Schema.define(version: 20170330203421) do
   end
 
   create_table "messages", force: :cascade do |t|
-    t.integer  "chatroom_id"
     t.integer  "user_id"
     t.text     "body"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id", using: :btree
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
   end
 
@@ -193,6 +191,5 @@ ActiveRecord::Schema.define(version: 20170330203421) do
 
   add_foreign_key "chatroom_users", "chatrooms"
   add_foreign_key "chatroom_users", "users"
-  add_foreign_key "messages", "chatrooms"
   add_foreign_key "messages", "users"
 end
