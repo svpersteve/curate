09ef14fc3e3713dbb12e7b79dcbc67accb8c1524
Steve Brewer <steve.v.brewer@gmail.com>
2017-03-30 22:11:49 +0100

    Create conversation views and controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,21 +1,42 @@
 class MessagesController < ApplicationController
-  load_and_authorize_resource
-  before_action :set_chatroom
+  before_action :find_conversation
+
+  def index
+    @messages = @conversation.messages
+    if @messages.length > 10
+      @over_ten = true
+      @messages = @messages[-10..-1]
+    end
+    if params[:m]
+      @over_ten = false
+      @messages = @conversation.messages
+    end
+    if @messages.last
+      if @messages.last.user_id != current_user.id
+        @messages.last.read = true;
+      end
+    end
+    @message = @conversation.messages.new
+  end
+
+  def new
+    @message = @conversation.messages.new
+  end
 
   def create
-    message = @chatroom.messages.new(message_params)
-    message.user = current_user
-    message.save
-    MessageRelayJob.perform_later(message)
+    @message = @conversation.messages.new(message_params)
+    if @message.save
+      redirect_to conversation_messages_path(@conversation)
+    end
   end
 
   private
 
-  def set_chatroom
-    @chatroom = Chatroom.find(params[:chatroom_id])
+  def message_params
+    params.require(:message).permit(:body, :user_id)
   end
 
-  def message_params
-    params.require(:message).permit(:body)
+  def find_conversation
+    @conversation = Conversation.find(params[:conversation_id])
   end
 end
