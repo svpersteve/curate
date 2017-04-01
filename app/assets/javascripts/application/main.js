6890d2deb34bbd8e64bbf77b2cbb0fd7d89e16dc
Steve Brewer <steve.v.brewer@gmail.com>
2017-04-01 14:04:04 +0100

    Wrap messages in scrollable container
    Also scroll to bottom when loading the page to show most recent message first.

--- app/assets/javascripts/application/main.js
+++ app/assets/javascripts/application/main.js
@@ -1,6 +1,12 @@
 $(document).on('turbolinks:load', function() {
+  messages = $('#messages')
   $('.chosen-select').chosen({
     allow_single_deselect: true,
     width: '400px'
   });
+  messages_to_bottom = function() {
+    messages.scrollTop(messages.prop("scrollHeight"))
+  };
+
+  messages_to_bottom()
 });
