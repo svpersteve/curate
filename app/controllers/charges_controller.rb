fbaa0bafaed80ab287039c36ec0c95aef7ae3b6c
Steve Brewer <steve.v.brewer@gmail.com>
2017-04-02 00:17:18 +0100

    Allow purchase of artwork directly from artist

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,25 +1,33 @@
 class ChargesController < ApplicationController
+  before_action :find_artwork, only: :create
+
   def new
   end
 
   def create
-    # Amount in pence
-    @amount = 500
+    @amount = @artwork.price
 
     customer = Stripe::Customer.create(
       :email => params[:stripeEmail],
       :source  => params[:stripeToken]
     )
 
-    charge = Stripe::Charge.create(
+    charge = Stripe::Charge.create({
       :customer    => customer.id,
       :amount      => @amount,
-      :description => 'Rails Stripe customer',
-      :currency    => 'usd'
-    )
+      :description => @artwork.name,
+      :currency    => 'gbp',
+      :application_fee => (@amount / 10)
+    }, :stripe_account => @artwork.artist.uid)
 
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to new_charge_path
+  end
+
+  private
+
+  def find_artwork
+    @artwork = Artwork.find(params[:format])
   end
 end
