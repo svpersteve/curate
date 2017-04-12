258419e0ec9809e0fcaf4fbcdff51322494977ad
Steve Brewer <steve.brewer@futurelearn.com>
2017-04-12 11:39:53 +0100

    Add artworks to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def index
     @featured_posts = Post.published.featured.order('published_at desc')
+    @featured_artworks = Artwork.curated.order('artwork_likes_count desc')
   end
 end
