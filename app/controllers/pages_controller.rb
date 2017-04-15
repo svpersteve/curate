92d7bab962c95b8fdac7342ee07a09407974e5b9
Steve Brewer <steve.brewer@futurelearn.com>
2017-04-15 17:07:20 +0100

    Add events banner to homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,10 @@ class PagesController < ApplicationController
     @featured_posts = Post.published.featured.order('published_at desc')
     @featured_artworks = Artwork.curated.order('artwork_likes_count desc')
     @homepage_wallpaper = Wallpaper.find_by(location: 'Homepage').image
+    @events_wallpaper = Wallpaper.find_by(location: 'Events banner').image
+  end
+
+  def events
+    @events_wallpaper = Wallpaper.find_by(location: 'Events banner').image
   end
 end
