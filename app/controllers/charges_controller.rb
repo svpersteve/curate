517aca62f91b7aecfa3d39b64fb1f6b3ffcd3940
Steve Brewer <steve.brewer@futurelearn.com>
2017-04-16 20:32:45 +0100

    wip

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -5,24 +5,35 @@ class ChargesController < ApplicationController
   end
 
   def create # rubocop:disable Metrics/MethodLength
-    @amount = (@artwork.price * 100)
+    user = @artwork.artist
+    amount = (@artwork.price * 100)
+    fee = (amount * 0.25)
 
-    customer = Stripe::Customer.create(
-      email: params[:stripeEmail],
-      source: params[:stripeToken]
-    )
+    begin
+      charge_attrs = {
+        amount: amount,
+        currency: 'gbp',
+        source: params[:token],
+        description: "Purchase of #{@artwork.name}",
+        application_fee: fee
+      }
 
-    Stripe::Charge.create({
-      customer: customer.id,
-      amount: @amount,
-      description: @artwork.name,
-      currency: 'gbp',
-      application_fee: (@amount / 10)
-    }, stripe_account: @artwork.artist.stripe_uid)
+      case params[:charge_on]
+      when 'connected'
+        charge = Stripe::Charge.create( charge_attrs, user.access_code )
+      when 'platform'
+        charge_attrs[:destination] = user.stripe_uid
+        charge = Stripe::Charge.create( charge_attrs )
+      end
 
-  rescue Stripe::CardError => e
-    flash[:error] = e.message
-    redirect_to new_charge_path
+      flash[:notice] = "Charged successfully! <a target='_blank' rel='#{params[:charge_on]}-account' href='https://dashboard.stripe.com/test/payments/#{charge.id}'>View in dashboard &raquo;</a>"
+
+    rescue Stripe::CardError => e
+      error = e.json_body[:error][:message]
+      flash[:error] = "Charge failed! #{error}"
+    end
+
+    redirect_to @artwork
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -57,6 +57,15 @@ Rails.application.routes.draw do
   get 'notifications/mark_all_read', to: 'notifications#mark_all_read', as: :mark_all_read
   resources :notifications, only: :index
 
+  # Stripe Connect endpoints
+  #  - oauth flow
+  get '/connect/oauth' => 'stripe#oauth', as: 'stripe_oauth'
+  get '/connect/confirm' => 'stripe#confirm', as: 'stripe_confirm'
+  get '/connect/deauthorize' => 'stripe#deauthorize', as: 'stripe_deauthorize'
+  #  - create accounts
+  post '/connect/managed' => 'stripe#managed', as: 'stripe_managed'
+  post '/connect/standalone' => 'stripe#standalone', as: 'stripe_standalone'
+
   namespace :admin do
     root to: 'users#index'
     resources :users
